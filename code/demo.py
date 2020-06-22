36b424581232aa932eba3d124c8c9be836a58807
Svante Sörberg <svantesorberg@outlook.com>
2020-06-22 14:07:50 +0200

    Lots of changes after playing around with CNN

--- code/demo.py
+++ code/demo.py
@@ -3,11 +3,14 @@ import tensorflow as tf
 import numpy as np
 
 EPOCHS = 5
-DATASETS = 5
+DATASETS = 3
 SEEDS = [42, 1337, 69, 420, 69420, 42069, 7331, 96, 42069420, 6969]
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+input_shape = (28, 28, 1)
 vec_len = 28*28
 
 #x_train90 = np.rot90(x_train, axes=(1,2), k=1)
@@ -15,24 +18,29 @@ vec_len = 28*28
 
 
 # Normalize
-x_train_original = x_train.reshape(x_train.shape[0], vec_len) / 255.0
-x_test_original = x_test.reshape(x_test.shape[0], vec_len) / 255.0
+#x_train_original = x_train.reshape(x_train.shape[0], vec_len) / 255.0
+#x_test_original = x_test.reshape(x_test.shape[0], vec_len) / 255.0
+x_train_original = x_train / 255.0
+x_test_original = x_test / 255.0
 
 datasets = [(x_train_original, x_test_original)]
 
 # Create n - 1 permutations
 for i in range(DATASETS - 1):
     perm = np.random.RandomState(SEEDS[i]).permutation(vec_len)
+
+    temp_train = x_train_original.reshape(x_train_original.shape[0], vec_len)
+    temp_test = x_test_original.reshape(x_test_original.shape[0], vec_len)
     datasets.append(
         (
-            np.array([x[perm] for x in x_train_original]),
-            np.array([x[perm] for x in x_test_original])
+            np.array([x[perm].reshape(input_shape) for x in temp_train]),
+            np.array([x[perm].reshape(input_shape) for x in temp_test])
         )
     )
 
 n_classes = len(np.unique(np.append(y_train, y_test)))
 
-net = EWC_Network(EPOCHS, 128, x_train_original.shape[1:], len(np.unique(np.append(y_train, y_test))))
+net = EWC_Network(EPOCHS, 32, x_train_original.shape[1:], len(np.unique(np.append(y_train, y_test))))
 
 y_train = tf.keras.utils.to_categorical(y_train, n_classes)
 y_test = tf.keras.utils.to_categorical(y_test, n_classes)
@@ -58,6 +66,6 @@ for i, dataset in enumerate(datasets[1:]):
 
 for i in range(1, DATASETS):
     net._train_model([i])
-    #net.evaluate()
+    net.evaluate()
 
-net.evaluate()
+#net.evaluate()
--- code/model.py
+++ code/model.py
@@ -1,8 +1,14 @@
 import tensorflow as tf
 import numpy as np
 import tensorflow.keras.backend as kb
+import multiprocessing
 
 from helpers import get_name_or_id
+# E.G with VGG16 (138 000 000 parameters), a batch size of 16 would require ~8-9GB RAM
+
+# FOR TESTING
+np.random.seed(1337)
+tf.random.set_seed(1337)
 
 class EWC_Network():
     
@@ -12,7 +18,9 @@ class EWC_Network():
         batch_size, 
         input_shape, 
         n_classes, 
-        learning_rate = 1e-3
+        learning_rate = 1e-4,
+        gradient_batch_size = 1,
+        ewc_lambda = 800 # Tune this
     ):
         self.tasks = []                         # Info/data for the tasks
         self.n_epochs = n_epochs                # Epochs used in training
@@ -21,10 +29,25 @@ class EWC_Network():
         self.input_shape = input_shape          # Data input shape
         self.n_classes = n_classes              # Number of output classes
 
+        # Parameters not specific to the problem
+        #   gradient_batch_size:
+        #       The number of data points to combine when calculating
+        #       the gradient that the empirical Fisher matrix is based on.
+        #       For a true empirical Fisher, it should be 1 - but this 
+        #       computation is rather slow. However, the norm of the gradient
+        #       is affected by the batch size, so changing it is not really
+        #       recommended unless other parameters, specifically 
+        #       ewc_lambda are also tuned.
+        #
+        #   ewc_lambda:
+        #       Multiplicative constant used when computing the regularization
+        #       term.
+        self.gradient_batch_size = gradient_batch_size 
+        self.ewc_lambda = ewc_lambda 
+
+
         self._build_model()
 
-    # TODO
-    #
     # This class represents the regularization function in EWC, i.e.
     # the penalty term associated with the current parameters based 
     # on how much they differ from previously trained parameters for 
@@ -46,7 +69,6 @@ class EWC_Network():
                         tf.multiply(fisher, tf.square(weights - params))
                     )
                 )
-
             return penalty
 
         # Add new parameters to the vector, used when a new task
@@ -68,49 +90,71 @@ class EWC_Network():
     def update_regularization_functions(self, task):
         # Updates the regularization functions for each layer,
         # adding learned parameters for the most recently learned task
+        j = 0
         for i, layer in enumerate(self.model.layers):
 
-            # TESTING
-            layer.kernel_regularizer.set_constant(0.1)
-            layer.bias_regularizer.set_constant(0.1)
+            if layer.trainable and len(layer.trainable_weights) > 0:
 
-            # Update weight parameters
-            new_parameters = task['trained_parameters'][2*i]
-            new_fisher = task['fisher_diagonal'][2*i]
-            layer.kernel_regularizer.add_new_parameters(new_parameters)
-            layer.kernel_regularizer.add_new_fisher_diagonal(new_fisher)
+                # Update weight parameters
+                new_parameters = task['trained_parameters'][2*j]
+                new_fisher = task['fisher_diagonal'][2*j]
+                layer.kernel_regularizer.add_new_parameters(new_parameters)
+                layer.kernel_regularizer.add_new_fisher_diagonal(new_fisher)
 
-            # Update bias parameters
-            new_parameters = task['trained_parameters'][2*i + 1]
-            new_fisher = task['fisher_diagonal'][2*i + 1]
-            layer.bias_regularizer.add_new_parameters(new_parameters)
-            layer.bias_regularizer.add_new_fisher_diagonal(new_fisher)
+                # Update bias parameters
+                new_parameters = task['trained_parameters'][2*j + 1]
+                new_fisher = task['fisher_diagonal'][2*j + 1]
+                layer.bias_regularizer.add_new_parameters(new_parameters)
+                layer.bias_regularizer.add_new_fisher_diagonal(new_fisher)
+                j += 1
+
     
-
-    # Build a fully connected network with two hidden layers
-    # Currently static width = 400
     def _build_model(self):
         self.model = tf.keras.models.Sequential([
-            tf.keras.layers.Input(shape=self.input_shape, name='input'),
-            tf.keras.layers.Dense(
-                600, 
+            #tf.keras.layers.Input(shape=self.input_shape, name='input'),
+            tf.keras.layers.Input(shape=(28, 28, 1), name='input'),
+            tf.keras.layers.Conv2D(
+                64, 
+                (3, 3),
+                padding='same',
+                bias_regularizer=self.EWC_Regularizer(constant=self.ewc_lambda),
+                kernel_regularizer=self.EWC_Regularizer(constant=self.ewc_lambda),
                 activation='relu',
-                bias_regularizer=self.EWC_Regularizer(),
-                kernel_regularizer=self.EWC_Regularizer(),
+                name='conv1-1'
+            ),
+            tf.keras.layers.Conv2D(
+                64, 
+                (3, 3),
+                padding='same',
+                bias_regularizer=self.EWC_Regularizer(constant=self.ewc_lambda),
+                kernel_regularizer=self.EWC_Regularizer(constant=self.ewc_lambda),
+                activation='relu',
+                name='conv1-2'
+            ),
+            tf.keras.layers.MaxPool2D(
+                pool_size=(2,2),
+                strides=(2,2)
+            ),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(
+                300, 
+                activation='relu',
+                bias_regularizer=self.EWC_Regularizer(constant=self.ewc_lambda),
+                kernel_regularizer=self.EWC_Regularizer(constant=self.ewc_lambda),
                 name='fc1'
             ),
             tf.keras.layers.Dense(
-                600, 
+                300, 
                 activation='relu', 
-                bias_regularizer=self.EWC_Regularizer(),
-                kernel_regularizer=self.EWC_Regularizer(), 
+                bias_regularizer=self.EWC_Regularizer(constant=self.ewc_lambda),
+                kernel_regularizer=self.EWC_Regularizer(constant=self.ewc_lambda), 
                 name='fc2'
             ),
             tf.keras.layers.Dense(
                 self.n_classes, 
                 activation='softmax',
-                bias_regularizer=self.EWC_Regularizer(),
-                kernel_regularizer=self.EWC_Regularizer(), 
+                bias_regularizer=self.EWC_Regularizer(constant=self.ewc_lambda),
+                kernel_regularizer=self.EWC_Regularizer(constant=self.ewc_lambda), 
                 name='output')
         ])
 
@@ -153,54 +197,96 @@ class EWC_Network():
                 task['Y_train'],
                 epochs = self.n_epochs,
                 batch_size=self.batch_size,
-                verbose=False
+                verbose=True
             )
 
             print('\rTraining on', get_name_or_id(task), 'done!')
-            
-            task['trained_parameters'] = self.model.get_weights()
+            #task['trained_parameters'] = self.model.get_weights()
 
-            # Fisher Matrix by sum
-            # print('\nComputing empirical Fisher - this may take a while')
-
-            # sums = [np.zeros(shape=(784, 800)), np.zeros(shape=(800,)), np.zeros(shape=(800, 800)), np.zeros(shape=(800,)), np.zeros(shape=(800, 10)), np.zeros(shape=(10,))]
-            # for i in range(0, task['X_train'].shape[0], 10):
-            #     progress = round(i / task['X_train'].shape[0] * 100)
-            #     print('\r[{0}{1}] {2}%'.format('#'*int(progress/10), ' '*(10 - int(progress/10)), progress), end='')
-            #     data = np.array([task['X_train'][i]])
-            #     labels = np.array([task['Y_train'][i]])
-
-
-
-            #     with tf.GradientTape() as tape:
-            #         tape.watch(self.model.trainable_weights)
-            #         predictions = self.model(data)
-            #         loss = tf.keras.losses.categorical_crossentropy(labels, predictions)
-            #     grads = tape.gradient(loss, self.model.trainable_weights)
-            #     squared_gradients = list(map(tf.square, grads))
-
-            #     for j in range(len(sums)):
-            #         sums[j] = sums[j] + squared_gradients[j]
-
-            # task['fisher_diagonal'] = sums
-            # print(' Done!')
-            
-            # Fisher matrix by single batch
-            data = task['X_train']
-            labels = task['Y_train']
-            with tf.GradientTape() as tape:
-                tape.watch(self.model.trainable_weights)
-                predictions = self.model(data)
-                loss = tf.keras.losses.categorical_crossentropy(labels, predictions)
-            grads = tape.gradient(loss, self.model.trainable_weights)
-
-            squared_gradients = list(map(lambda x: x**2, grads))
-            task['fisher_diagonal'] = squared_gradients
+            self.store_trained_parameters(task)
+            self.compute_fisher(task)
 
             # After model has been trained, we need to recompile it
             # in order to use the updated the regularization function
             self.update_regularization_functions(task)
             self._compile_model()
+
+    def store_trained_parameters(self, task):
+        task['trained_parameters'] = [
+            tf.convert_to_tensor(np.copy(weight)) 
+            for weight in self.model.get_weights()
+        ]
+        
+
+    def compute_fisher(self, task):
+        # Fisher matrix by batches
+        print('\nComputing empirical Fisher - this may take a while')
+        data = task['X_train']
+        labels = task['Y_train']
+
+        num_splits = int(data.shape[0] / self.gradient_batch_size)
+        data_split = np.array_split(data, num_splits)
+        label_split = np.array_split(labels, num_splits)
+
+        # Determine the shape of the returned gradient and initialize
+        # data structure that will hold the cumulative gradients data
+        # w.r.t. the batches 
+        sums = [
+            np.zeros(shape = w.shape) for w in self.model.trainable_weights
+        ]
+
+        for i, (batch_data, batch_label) in enumerate(
+            zip(data_split, label_split)
+        ):
+            progress = round(i / num_splits * 100)
+            print('\r[{0}{1}] {2}%'.format('#'*int(progress/10), ' '*(10 - int(progress/10)), progress), end='')
+            with tf.GradientTape() as tape:
+                tape.watch(self.model.trainable_weights)
+                predictions = self.model(batch_data)
+                # Calculates loss for each data point in the batch
+                loss = tf.keras.losses.categorical_crossentropy(
+                    batch_label, 
+                    predictions
+                )
+
+            # Using tape.gradients() directly on the loss tensor will
+            # sum all the gradients together, which may not be ideal,
+            # but speeds up computation. Another consequence of this
+            # is that the batch size in the gradient computation
+            # affects what the value lambda in the regularization 
+            # function should be. We try to avoid this by dividing the 
+            # gradient by the batch size, thus getting the 
+            # "average gradient" for that batch.
+            # For an exact (empirical) Fisher, use a batch size of 1.
+            gradient = tape.gradient(
+                loss, 
+                self.model.trainable_weights
+            )
+
+            # Add the square batch_size times to compensate,
+            # This may be a bit "cheaty" but at least we get 
+            # values that are similar regardless of batch size,
+            # which means we shouldn't have to adjust lambda too much
+            sums = [
+                s + 
+                (
+                    (gradient[j])**2
+                )
+                for j, s in enumerate(sums)
+            ]
+
+        # Divide by number of data points to normalize, note that 
+        # this normalization will not yield comparable results 
+        # for different batch sizes!
+        task['fisher_diagonal'] = [s / num_splits for s in sums]
+        # print(
+        #     'Fisher sum:',
+        #     list(map(np.sum, task['fisher_diagonal']))
+        # )
+        # print(
+        #     'Fisher average:',
+        #     list(map(np.average, task['fisher_diagonal']))
+        # )
 
     def add_task(
         self, 
